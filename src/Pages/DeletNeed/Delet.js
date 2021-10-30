3f17d23e36052b2b47fb0e45d835e440af299059
mdhainf20 <mdhanif115825@gmail.com>
2021-10-30 22:38:47 +0600

    orna header added

---
+++ src/Pages/DeletNeed/Delet.js
@@ -0,0 +1,3 @@
+const logo = {
+    img:"https://i.ibb.co/Qdg1mY6/logo.png"
+}
---
+++ src/Pages/Home/Header/Header.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+
+const Header = () => {
+    const [logos, setLogos] = useState([]);
+    // const [balisUrl,setBalisUrl] = useState({});
+    axios.get('http://localhost:5000/logo')
+    .then(res=>{
+        setLogos(res.data);
+        // console.log(res.data);
+    })
+    
+    return (
+        <div>
+
+            
+            <Navbar bg="light" expand="lg">
+                <Container>
+                <Navbar.Brand href="#home">
+                {
+                        logos.map(logo => <img
+                            key={logo._id}
+                            alt=""
+                            src= {logo.img}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                            /> )
+                            
+                    }
+                </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link as={Link} to="/home" href="#home">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/link" href="#link">Link</Nav.Link>
+                        {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown> */}
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
