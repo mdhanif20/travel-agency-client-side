f096b729e89f21dab79af06855e911f7f70cb235
mdhainf20 <mdhanif115825@gmail.com>
2021-10-31 11:51:57 +0600

    service area added

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaFacebookF } from 'react-icons/fa';
+import { AiFillYoutube } from 'react-icons/ai';
+import { BsTwitter } from 'react-icons/bs';
+import { AiFillInstagram } from 'react-icons/ai';
 import "./Services.css";
 
 const Services = () => {
     const [services,setServices] = useState([]);
-    axios.get("http://localhost:5000/services")
+    axios.get("https://nameless-savannah-22070.herokuapp.com/services")
     .then(res =>{
         setServices(res.data);
     })
@@ -13,8 +17,9 @@ const Services = () => {
         <div>
             
             <div className="row px-md-5 py-md-5 py-3">
-                <div className="col-md-9 px-md-5 px-1" id="service-area">
-                    <h2 className="text-start text-bolder pb-3"> <small>Popular Destinations</small> </h2>
+                <h2 className="pb-5">Our Services</h2>
+                <div className="col-md-9 col-8 px-md-5 ps-4" id="service-area">
+                    <h2 className="text-start text-bolder pb-3"> <small  id="services">Popular Destinations</small> </h2>
                     {
                         services.map(service => <div className="card mb-3 rounded-0"
                         key={service._id} id="service-card">
@@ -26,7 +31,7 @@ const Services = () => {
                             <div className="card-body">
                               <h5 className="card-title">{service.name}</h5>
                               <p className="card-text"><small className="text-muted">{service.time}</small></p>
-                              <p className="card-text">{service.desciption.slice(0,130)}....</p>
+                              <p className="card-text">{service.desciption.slice(0,100)}....</p>
                              <Link to="/booking">
                              <button type="button" className="btn btn-primary border-0 rounded-0 fw-bolder">Book Now</button>
                              </Link>
@@ -36,8 +41,47 @@ const Services = () => {
                       </div>)
                     }
                 </div>
-                <div className="col-md-3">
+                <div className="col-md-3 col-4">
+                    <h3 className="text-start pb-1"> <small id="services">Follow Us</small> </h3>
+                    <div className="pt-3">
+                        <p id="upper-icon"> 
 
+                        <span id="facebook" className="fb-icon fw-bolder"><FaFacebookF className="fs-4"/> <span className="px-1">Follwers 9.8k</span> </span> 
+
+                        <span id="youtube" className="fw-bolder"><AiFillYoutube className="fs-4"/> <span className="px-2">Subscrib 2.3M</span> </span> 
+                        
+                         <span id="twiter" className="fw-bolder"><BsTwitter className="fs-4"/> <span className="px-1">Subscrib 2.3M</span> </span> 
+
+                         <span id="instagram" className="fw-bolder"><AiFillInstagram className="fs-4"/> <span className="px-1">Follwers 11.2k</span> </span> 
+                        </p>
+                    </div>
+                    <div>
+                        <h4 className="text-start border-bottom pb-2 pt-5">All Service</h4>
+                        {
+                            services.map(service =><div key={service._id} className="card mb-3 border-0">
+                            <div className="row g-0" id="all-service">
+                              <div className="col-md-4 col-4">
+                                <img id="allService-img" src={service.img2} className="img-fluid rounded-start" alt="..."/>
+                              </div>
+                              <div className="col-md-8 col-8 ps-2">
+                                <div className="card-body p-0 text-start">
+                                  <h6 className="card-title mb-0" id="all-service-header">{service.name}</h6>
+                                  <p className="card-text" id="allServiceDes">
+                                      {service.desciption.slice(0,50)}</p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>)
+                        }
+                    </div>
+                    <div>
+                    <h4 className="text-start border-bottom pb-2 pt-3">Contact Us</h4>
+                    <form action="#" id="form">
+                        <input type="email" placeholder="Email" />
+                        <textarea name="" id="" placeholder="Write your comment..."></textarea> <br />
+                        <input className="btn btn-primary fw-bolder" type="Submit" value="Send" />
+                    </form>
+                    </div>
                 </div>
             </div>
         </div>
