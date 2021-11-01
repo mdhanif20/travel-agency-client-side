470c9f116a010f00ad95355ca7cefb5102371e74
mdhainf20 <mdhanif115825@gmail.com>
2021-11-01 12:34:18 +0600

    added login area

--- src/Pages/LogIn/LogIn.js
+++ src/Pages/LogIn/LogIn.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import useFirebase from './../Firebase/useFirebase';
-
+import { FcGoogle } from 'react-icons/fc';
+import "./LogIn.css";
 const Login = () => {
     const {singInUsingGoogle} = useFirebase();
     return (
-        <div>
-            <h2>Login</h2>
+        <div id="loginForm" className="m-auto p-3 rounded border my-5">
+            <h2 className="pb-3">Sing In</h2>
             <form action="">
-                <input type="text" placeholder="Name" />
-                <input type="password" placeholder="Password" />
-                <input type="password" placeholder="re-type password" />
-                <input type="submit" value="Sing In" />
+                <input type="text" placeholder="Name" className="w-100 border rounded my-1" /> <br />
+                <input type="password" className="w-100 border rounded my-1"  placeholder="Password" /> <br />
+                <input type="password" className="w-100 border rounded my-1" placeholder="re-type password" /> <br />
+                <input type="submit" className="w-100 border rounded my-1 fw-bolder" value="Sing In" /> 
             </form>
-            <button onClick={singInUsingGoogle} className="btn btn-primary fw-bolder">Sing In</button>
+            <p className="my-3">--------Or--------</p>
+            <button onClick={singInUsingGoogle} className="btn fs-5 border w-100"><FcGoogle className="fs-4"/> Google Sing In</button>
         </div>
     );
 };
