e39f337b852df3a87b9d3eaeba53fe2f887c53ac
mdhainf20 <mdhanif115825@gmail.com>
2021-11-01 13:01:31 +0600

    added css in manage order area

--- src/Pages/ManageBooking/AllCustomer/AllCustomer.js
+++ src/Pages/ManageBooking/AllCustomer/AllCustomer.js
@@ -21,15 +21,15 @@ const AllCustomer = (props) => {
         <div id="customers" className="rounded my-3 p-3">
             <div className="text-start ps-2" id="customerFirst">
                 <h5>Name: {name}</h5>
-                <h6>Email: {email}</h6>
+                <h6  className="pt-1">Email: {email}</h6>
             </div>
             
            <div className="text-start ps-2"  id="customersecond">
                 <h6>Phone: {phone}</h6>
-                <h6>Tour Place: {tour}</h6>
+                <h6  className="pt-1">Tour Place: {tour}</h6>
            </div>
            <div className="text-start ps-2" id="customerThird">
-                <button onClick={()=>handleDeletUser(_id)}className="btn btn-primary">Delete</button>
+                <button onClick={()=>handleDeletUser(_id)}className="btn btn-primary  py-1">Delete</button>
            </div>
             
         </div>
